2b8f2115e358301ef14ab6aefad49ad93b02c52d
Frank Ebel <git@ebel.page>
2022-12-03 18:20:20 +0100

    refactor(day03): use "string.ascii_letters"

--- day03/main.py
+++ day03/main.py
@@ -1,33 +1,18 @@
+import string
+
 data = open("input.txt", "r").read().strip().split()
 
-
-def item_to_num(string):
-    res = ord(string)
-    res = res - ord("A") + 27 if string.isupper() else res - ord("a") + 1
-    return res
-
-
 # Part 1
-same_item = []
+p1 = 0
 for rucksack in data:
-    split = len(rucksack)//2
-    compartment1 = rucksack[:split]
-    compartment2 = rucksack[split:]
-    item = set.intersection(set(compartment1), set(compartment2)).pop()
-    same_item.append(item)
-part1 = sum(item_to_num(item) for item in same_item)
+    c1, c2 = rucksack[:len(rucksack)//2], rucksack[len(rucksack)//2:]
+    p1 += string.ascii_letters.index((set(c1) & set(c2)).pop()) + 1
 
 # Part 2
-same_group = []
+p2 = 0
 for i in range(0, len(data), 3):
-    rucksack1 = data[i]
-    rucksack2 = data[i+1]
-    rucksack3 = data[i+2]
-    item = set.intersection(set(rucksack1), set(rucksack2),
-                            set(rucksack3)).pop()
-    same_group.append(item)
-part2 = sum(item_to_num(item) for item in same_group)
+    r1, r2, r3 = data[i:i+3]
+    p2 += string.ascii_letters.index((set(r1) & set(r2) & set(r3)).pop()) + 1
 
-
-print(f"Solution Part 1: {part1}")
-print(f"Solution Part 2: {part2}")
+print(f"Solution Part 1: {p1}")
+print(f"Solution Part 2: {p2}")
