f17c45ab8c91c2a3d8bfc6989a86b2f90d530b60
Frank Ebel <git@ebel.page>
2022-12-03 01:07:29 +0100

    refactor(day02): use dictionaries as lookup table

--- day02/main.py
+++ day02/main.py
@@ -1,19 +1,13 @@
 with open("input.txt", "r") as f:
     data = f.read().strip().split("\n")
 
-# Part 1
-mapping1 = {"A": 1, "B": 2, "C": 3, "X": 1, "Y": 2, "Z": 3}
-dwl1 = {0: 3, 1: 6, 2: 0}  # 0 is draw, 1 is win, 2 is loss
-data1 = [[mapping1[game[0]], mapping1[game[2]]] for game in data]
-shape1 = [game[-1] for game in data1]
-outcome1 = [dwl1[(game[1] - game[0]) % 3] for game in data1]
+part1 = {"A X": 1+3, "A Y": 2+6, "A Z": 3+0,
+         "B X": 1+0, "B Y": 2+3, "B Z": 3+6,
+         "C X": 1+6, "C Y": 2+0, "C Z": 3+3}
 
-# Part 2
-mapping2 = {"A": 1, "B": 2, "C": 3, "X": 0, "Y": 3, "Z": 6}
-mp = {"X": -1, "Y": 0, "Z": 1}
-# "+ 2 mod 3 + 1" to map 4 -> 1, 0 -> 3
-shape2 = [(mapping2[game[0]] + mp[game[2]] + 2) % 3 + 1 for game in data]
-outcome2 = [mapping2[game[2]] for game in data]
+part2 = {"A X": 3+0, "A Y": 1+3, "A Z": 2+6,
+         "B X": 1+0, "B Y": 2+3, "B Z": 3+6,
+         "C X": 2+0, "C Y": 3+3, "C Z": 1+6}
 
-print(f"Solution Part 1: {sum(outcome1) + sum(shape1)}")
-print(f"Solution Part 2: {sum(outcome2) + sum(shape2)}")
+print(f"Solution Part 1: {sum(part1[game] for game in data)}")
+print(f"Solution Part 2: {sum(part2[game] for game in data)}")
