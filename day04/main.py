8c8acaa6bd887b835a64b127129c49e560da2e61
Frank Ebel <git@ebel.page>
2022-12-04 22:59:09 +0100

    refactor(day04): use sets

--- day04/main.py
+++ day04/main.py
@@ -1,25 +1,16 @@
 # https://adventofcode.com/2022/day/4
-import numpy as np
 
 with open("input.txt", "r") as f:
     data = [[int(element) for element in
              row.strip().replace("-", ",").split(",")]
             for row in f.readlines()]
 
-# Part 1
-p1 = 0
-for row in data:
-    d1, d2 = row[2] - row[0], row[3] - row[1]
-    if np.sign(d1) != np.sign(d2):
-        p1 += 1
-    if d1 == d2 == 0:
-        p1 += 1
-
-# Part 2
-p2 = 0
+p1, p2 = 0, 0
 for row in data:
     s1, s2 = set(range(row[0], row[1]+1)), set(range(row[2], row[3]+1))
-    if len(set(s1) & set(s2)) != 0:
+    if s1.issubset(s2) or s2.issubset(s1):
+        p1 += 1
+    if not s1.isdisjoint(s2):
         p2 += 1
 
 print(f"Solution Part 1: {p1}")
