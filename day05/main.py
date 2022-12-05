3b9206f9d98cb12f480e6e1c5c0658ac62faa7ab
Frank Ebel <git@ebel.page>
2022-12-06 00:43:36 +0100

    refactor(day05): shorten code

--- day05/main.py
+++ day05/main.py
@@ -9,12 +9,10 @@ stack_text, moves_text = data[:data.index("")], data[data.index("") + 1:]
 
 # Create stacks
 stack_text = stack_text[::-1]
-num_stacks = int(stack_text[0].split()[-1])
-stacks = []
-for i in range(num_stacks):
-    stack = [row[4*i+1] for row in stack_text[1:]]  # Extract crates
-    stack = [item for item in stack if item != " "]  # Remove empty
-    stacks.append(stack)
+stacks = [[crate for row in stack_text[1:] if (crate := row[4*i+1]) != " "]
+          for i in range(int(len(stack_text[0].split())))]
+# Add dummy to set starting index to 1
+stacks.insert(0, [""])
 
 # Create moves
 moves = [[int(number) for number in row.split()[1::2]] for row in moves_text]
@@ -24,11 +22,11 @@ stacks1, stacks2 = copy.deepcopy(stacks), copy.deepcopy(stacks)
 for move in moves:
     n, src, dest = move
     # Part 1
-    stacks1[dest-1].extend(stacks1[src-1][-1:-n-1:-1])  # Add crates
-    stacks1[src-1] = stacks1[src-1][:-n]  # Remove crates
+    stacks1[dest].extend(stacks1[src][-n:][::-1])  # Add crates
+    del stacks1[src][-n:]  # Remove crates
     # Part 2
-    stacks2[dest-1].extend(stacks2[src-1][-n:])  # Add crates
-    stacks2[src-1] = stacks2[src-1][:-n]  # Remove crates
+    stacks2[dest].extend(stacks2[src][-n:])  # Add crates
+    del stacks2[src][-n:]  # Remove crates
 
 # Combine top crates from each stack
 p1 = "".join([stack[-1] for stack in stacks1])
