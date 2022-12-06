fbe9879814e12f9c9c6ed81650cba9c22658116c
Frank Ebel <git@ebel.page>
2022-12-06 22:54:56 +0100

    refactor(day06): use more_itertools.sliding_window

--- day06/main.py
+++ day06/main.py
@@ -1,20 +1,17 @@
 # https://adventofcode.com/2022/day/6
 
+import more_itertools
+
+
 def find_distinct(string, length):
-    for i, _ in enumerate(string):
-        chars = string[i:i+length]
-        if len(set(chars)) == len(chars):
-            return i+length
+    for i, window in enumerate(more_itertools.sliding_window(string, length)):
+        if len(set(window)) == length:
+            return i + length
 
 
-data = open("input.txt", "r").read().strip().split("\n")
-
-p1, p2 = -1, -1  # Fallback
-for line in data:
-    # Part 1
-    p1 = find_distinct(line, 4)
-    # Part 2
-    p2 = find_distinct(line, 14)
+data = open("input.txt", "r").readline().strip()
+p1 = find_distinct(data, 4)
+p2 = find_distinct(data, 14)
 
 print(f"Solution Part 1: {p1}")
 print(f"Solution Part 2: {p2}")
