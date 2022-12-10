90f0adc67755862373d0b3ac138f1296aa593a38
Frank Ebel <git@ebel.page>
2022-12-10 13:22:04 +0100

    refactor(day10): clean up code

--- day10/main.py
+++ day10/main.py
@@ -1,7 +1,18 @@
 # https://adventofcode.com/2022/day/10
 
+import textwrap
+
+
+def update(cycle, raster, width_CRT, signal_strengths):
+    raster += "#" if abs((cycle % width_CRT)-X) <= 1 else "."
+    cycle += 1
+    signal_strengths.append(cycle*X)
+    return cycle, raster
+
+
 cycle = 0
 X = 1  # Register
+width_CRT = 40
 signal_strengths = []
 raster = ''
 
@@ -10,18 +21,12 @@ data = open("input.txt", "r").read().strip().split("\n")
 for instruction in data:
     match instruction.split():
         case ["noop"]:
-            raster += "#" if abs((cycle % 40)-X) <= 1 else "."
-            cycle += 1
-            signal_strengths.append(cycle*X)
+            cycle, raster = update(cycle, raster, width_CRT, signal_strengths)
         case "addx", num:
             # Cycle 1
-            raster += "#" if abs((cycle % 40)-X) <= 1 else "."
-            cycle += 1
-            signal_strengths.append(cycle*X)
+            cycle, raster = update(cycle, raster, width_CRT, signal_strengths)
             # Cycle 2
-            raster += "#" if abs((cycle % 40)-X) <= 1 else "."
-            cycle += 1
-            signal_strengths.append(cycle*X)
+            cycle, raster = update(cycle, raster, width_CRT, signal_strengths)
             X += int(num)
 
 # Part 1
@@ -29,5 +34,5 @@ p1 = sum(signal_strengths[19::40])
 print("Solution Part 1:", p1)
 
 # Part 2
-p2 = [raster[i:i+40] for i in range(0, len(raster), 40)]
+p2 = textwrap.wrap(raster, width_CRT)
 print("Solution Part 2:", *p2, sep="\n")
