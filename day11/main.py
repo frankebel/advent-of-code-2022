53cde5e0fc1d568bb61314ebe4065a9496b88e16
Frank Ebel <git@ebel.page>
2022-12-11 22:13:49 +0100

    feat(day11): clean up code

--- day11/main.py
+++ day11/main.py
@@ -4,6 +4,30 @@ import copy
 import math
 import re
 
+
+def run_rounds(monkeys: dict, rounds: int, relief: int) -> int:
+    monkeys = copy.deepcopy(monkeys)
+    for monkey in monkeys.values():
+        monkey["inspections"] = 0
+    # Loop over rounds
+    for _ in range(rounds):
+        for monkey, stats in monkeys.items():
+            for item in stats["items"]:
+                worry = stats["operation"](item)
+                # Hard coded p1, p2.
+                worry = worry // relief if relief == 3 else worry % relief
+                if worry % stats["divisibility"] == 0:
+                    monkeys[stats["monkey_true"]]["items"].append(worry)
+                else:
+                    monkeys[stats["monkey_false"]]["items"].append(worry)
+            stats["inspections"] += len(stats["items"])
+            stats["items"] = []
+
+    # Find two highest inspections and return their product.
+    inspections = [monkey["inspections"] for monkey in monkeys.values()]
+    return math.prod(sorted(inspections, reverse=True)[:2])
+
+
 rounds1 = 20
 rounds2 = 10_000
 monkeys = dict()
@@ -21,12 +45,16 @@ for monkey in data:
     # Starting items
     starting_items = re.findall(r"\d+", lines[1])
     starting_items = [int(item) for item in starting_items]
-    m["items1"] = copy.deepcopy(starting_items)
-    m["items2"] = copy.deepcopy(starting_items)
+    m["items"] = starting_items
     # Operation
-    operation = lines[2].split("= ")[-1]
-    # m["operation"] = lambda x: eval(operation.replace("old", str(x)))
-    m["operation"] = operation
+    operation = lines[2].split("= ")[-1].split()
+    match operation:
+        case "old", "*", "old":
+            m["operation"] = lambda x: x*x
+        case "old", "*", n:
+            m["operation"] = lambda x, n=int(n): x*n
+        case "old", "+", n:
+            m["operation"] = lambda x, n=int(n): x+n
     # Divisibility test
     divisibility = re.search(r"\d+", lines[3])
     if divisibility:
@@ -40,49 +68,12 @@ for monkey in data:
     if monkey_false:
         m["monkey_false"] = int(monkey_false[0])
 
-    m["inspections1"] = 0  # For part 1
-    m["inspections2"] = 0  # For part 2
-
     # Append monkey to monkeys dictionary.
     monkeys[num] = m
 
-# For part 2
-p2_num = math.lcm(*[monkey["divisibility"] for monkey in monkeys.values()])
-
-
-# Play all rounds
-for round in range(rounds2):
-    # Play a single round
-    for monkey, stats in monkeys.items():
-        # Part 1
-        if round < rounds1:
-            for item in stats["items1"]:
-                worry1 = eval(stats["operation"].replace(
-                    "old", str(item))) // 3
-                if worry1 % stats["divisibility"] == 0:
-                    monkeys[stats["monkey_true"]]["items1"].append(worry1)
-                else:
-                    monkeys[stats["monkey_false"]]["items1"].append(worry1)
-            stats["inspections1"] += len(stats["items1"])
-            stats["items1"] = []
-
-        # Part 2
-        for item in stats["items2"]:
-            worry2 = eval(stats["operation"].replace(
-                "old", str(item))) % p2_num
-            if worry2 % stats["divisibility"] == 0:
-                monkeys[stats["monkey_true"]]["items2"].append(worry2)
-            else:
-                monkeys[stats["monkey_false"]]["items2"].append(worry2)
-        stats["inspections2"] += len(stats["items2"])
-        stats["items2"] = []
 
 # Part 1
-inspections = [monkey["inspections1"] for monkey in monkeys.values()]
-p1 = math.prod(sorted(inspections, reverse=True)[:2])
-print("Solution Part 1:", p1)
-
+print("Solution Part 1:", run_rounds(monkeys, rounds1, 3))
 # Part 2
-inspections = [monkey["inspections2"] for monkey in monkeys.values()]
-p2 = math.prod(sorted(inspections, reverse=True)[:2])
-print("Solution Part 2:", p2)
+p2_num = math.lcm(*[monkey["divisibility"] for monkey in monkeys.values()])
+print("Solution Part 2:", run_rounds(monkeys, rounds2, p2_num))
